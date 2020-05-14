03d73b12c484354cd0827b1f9716d1361e036fac
전우성 <wjsdbwls5555@gmail.com>
2020-05-14 17:50:45 +0900

    get milk add

--- milk buy.js
+++ milk buy.js
@@ -27,3 +27,42 @@ function getMilk(money) {           //------>함수 getMilk라는이름을 부
     console.log("enterHouse");
 }
 getMilk(5);  //------->MiLK를 사는데 5$를 부여해줌.
+
+//------------------------------------------------------------------------------
+function getMilk(money, costPerBottle) {    // 10$로 1.5$짜리를 사러감        
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    console.log(" buy " + calcBottles(money, costPerBottle) + " bottles of milk"); //10$로 1.5$짜리 6병을삼
+
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+
+    return calcChange(money, 1.5); //===== 0원의 거스름돈
+
+}
+function calcBottles(startingMoney, costPerBottle) {
+    var numberOfBottles = Math.floor(startingMoney / costPerBottle);
+    return numberOfBottles;                //=====(10$ / 1.5$= 반내림 하고 6)
+}
+
+function calcChange(startingAmount, costPerBottle) {
+    var change = startingAmount % costPerBottle;
+    return change;                                //거스름돈 반환 (10 % 2.5 == 0 )
+}
+console.log("Hello master, here is your " + getMilk(10, 1.5) + "change.");           
